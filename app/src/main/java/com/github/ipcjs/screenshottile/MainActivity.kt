fa037957d3aa246ec7f210a414b9f28e868aa603
cvzi <cuzi@openmail.cc>
2019-03-10 22:37:11 +0100

    Tutorial activity  * slide through images  * settings button style/color

--- app/src/main/java/com/github/ipcjs/screenshottile/MainActivity.kt
+++ app/src/main/java/com/github/ipcjs/screenshottile/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.github.ipcjs.screenshottile
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.github.ipcjs.screenshottile.dialog.ContainerActivity
 import com.github.ipcjs.screenshottile.dialog.SettingFragment
+
 
 /**
  * Shows a how-to video.
@@ -28,32 +35,64 @@ class MainActivity : Activity() {
         R.drawable.screenshot_12,
         R.drawable.screenshot_13,
         R.drawable.screenshot_14,
-        R.drawable.screenshot_15
+        R.drawable.screenshot_15,
+        R.drawable.screenshot_16
     )
-    private var index = 0
-    private var nextImage: Int
-        get() {
-            index = (index + 1) % images.size
-            return images[index]
-        }
-        set(newIndex) {
-            index = newIndex % images.size
-        }
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var textViewStep: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        actionBar?.title = getString(R.string.app_name) + " " +  getString(R.string.tutorial)
-        val textViewStep = findViewById<TextView>(R.id.textViewStep)
-        findViewById<ImageView>(R.id.imageView).apply {
-            setOnClickListener {
-                val imageView = it as ImageView
-                imageView.setImageResource(nextImage)
-                textViewStep.text = (index + 1).toString()
-            }
+
+        textViewStep = findViewById(R.id.textViewStep)
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = TutorialPagerAdapter()
+        viewPager.setOnClickListener {
+            viewPager.setCurrentItem(viewPager.currentItem + 1 % images.size, true)
         }
+        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(position: Int) {
+                textViewStep.text = (position + 1).toString()
+            }
+        })
+
         findViewById<Button>(R.id.buttonSettings)?.setOnClickListener {
             ContainerActivity.start(this@MainActivity, SettingFragment::class.java)
         }
     }
+
+    private inner class ClickableImageView(context: Context?) : ImageView(context) {
+        init {
+            setOnClickListener {
+                viewPager.setCurrentItem((viewPager.currentItem + 1) % images.size, true)
+            }
+        }
+    }
+
+    private inner class TutorialPagerAdapter : PagerAdapter() {
+        override fun isViewFromObject(view: View, obj: Any): Boolean {
+            return view == obj
+        }
+
+        override fun getCount(): Int {
+            return images.size
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            return ClickableImageView(this@MainActivity).apply {
+                setImageResource(images[position])
+                (container as ViewPager).addView(this, 0)
+            }
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
+            (container as? ViewPager)?.removeView(obj as? View)
+        }
+    }
+
 }
