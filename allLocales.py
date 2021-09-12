22dd7d9b7a4fb7c9639482e2ab54f58315881669
cvzi <cuzi@openmail.cc>
2021-09-12 13:42:47 +0200

    alllocales

--- allLocales.py
+++ allLocales.py
@@ -1,51 +1,5 @@
-locales = [
-    "ar",
-    "ar-rSA",
-    "bn",
-    "de-rDE",
-    "el-rGR",
-    "es",
-    "es-rES",
-    "fa",
-    "fr-rFR",
-    "gu",
-    "hi",
-    "hi-rIN",
-    "hu-rHU",
-    "hy-rAM",
-    "in",
-    "it-rIT",
-    "iw",
-    "ja",
-    "kn",
-    "mr",
-    "ms",
-    "nb",
-    "nl-rNL",
-    "no",
-    "or",
-    "pa",
-    "pl",
-    "pl-rPL",
-    "pt-rBR",
-    "pt-rPT",
-    "ro-rRO",
-    "ru",
-    "ru-rRU",
-    "sw",
-    "ta",
-    "te",
-    "th",
-    "tl",
-    "tr-rTR",
-    "ug",
-    "uk-rUA",
-    "ur",
-    "vi-rVN",
-    "zh",
-    "zh-rCN",
-    "zh-rHK",
-    "zh-rTW"]
+import os
+import re
 
 keywords = [
     # https://github.com/JetBrains/kotlin/blob/v1.4.10/core/descriptors/src/org/jetbrains/kotlin/renderer/KeywordStringsGenerated.java
@@ -79,12 +33,25 @@ keywords = [
     "typeof"
 ]
 
+locales = []
+
 
 def methodName(locale):
     x = locale.replace('-r', '').replace('-', '')
     while x in keywords:
         x += "99"
     return x
+
+
+print('            - "en-US"')
+for filename in os.listdir("app/src/main/res"):
+    m = re.match("^values-([a-z]{2})-r([A-Z]{2})$", filename)
+    m2 = re.match("^values-([a-z]{2})$", filename)
+    if m:
+        print('            - "%s-%s"' % (m.group(1), m.group(2)))
+
+    if m or m2:
+        locales.append(filename.split("values-")[1])
 
 
 for locale in locales:
