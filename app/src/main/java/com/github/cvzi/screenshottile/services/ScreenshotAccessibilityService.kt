71ff3ebbc3a3ebf477b17486a46b2bd1b9913db7
cvzi <cuzi@openmail.cc>
2024-05-31 01:31:11 +0200

    Floating button x,y relative to view on Android 14+
    #462

--- app/src/main/java/com/github/cvzi/screenshottile/services/ScreenshotAccessibilityService.kt
+++ app/src/main/java/com/github/cvzi/screenshottile/services/ScreenshotAccessibilityService.kt
@@ -313,7 +313,7 @@ class ScreenshotAccessibilityService : AccessibilityService() {
                         if (!dragDone) {
                             val x: Int
                             val y: Int
-                            if (event.action == DragEvent.ACTION_DROP) {
+                            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU || event.action == DragEvent.ACTION_DROP) {
                                 // x and y are relative to the inside of the view's bounding box
                                 val old = App.getInstance().prefManager.floatingButtonPosition
                                 x = (old.x - v.measuredWidth / 2.0 + event.x).toInt()
