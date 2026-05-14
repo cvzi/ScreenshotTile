da7898b21cf31e16f21e05f1628296ac79b626fe
cvzi <cuzi@openmail.cc>
2026-05-14 14:06:42 +0200

    Code style

--- app/src/main/java/com/github/cvzi/screenshottile/activities/WebViewActivity.kt
+++ app/src/main/java/com/github/cvzi/screenshottile/activities/WebViewActivity.kt
@@ -2,9 +2,7 @@ package com.github.cvzi.screenshottile.activities
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
